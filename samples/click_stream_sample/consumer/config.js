c9aff5eace1609ccb036b2dc1f44b5c15223eb72
Leon Kogan <leon@translationexchange.com>
2015-08-12 15:43:57 -0700

    no message

--- samples/click_stream_sample/consumer/config.js
+++ samples/click_stream_sample/consumer/config.js
@@ -18,20 +18,20 @@ permissions and limitations under the License.
 var config = module.exports = {
   s3 : {
     // Region for Amazon S3. Defaults to us-east-1.
-    // region : '',
+    // region : 'us-west-1',
 
     // Amazon S3 bucket to store batched clickstream data. The consumer application
     // may create a new bucket (based on S3.createBucketIfNotPresent value),
     // if the specified bucket doesn't exist.
-    bucket : 'kinesis-clickstream-batchdata',
+    bucket : 'trex-analyst',
 
     // Enables the consumer application to create a new S3 bucket if the specified
     // bucket doesn't exist.
-    createBucketIfNotPresent : true
+    createBucketIfNotPresent : false
   },
 
   clickStreamProcessor : {
     // Maximum batch size in bytes before sending data to S3.
-    maxBufferSize : 1024 * 1024
+    maxBufferSize : 10 * 1024
   }
 };
